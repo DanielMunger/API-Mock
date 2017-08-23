3091cddb57ad9d8a8485ab02052dc9c84ff85e6e
Daniel Munger <mungerda@gmail.com>
2017-08-23 14:10:20 -0700

    adds category class.

---
+++ src/API-4TELL/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_4TELL.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+
+    } 
+}
