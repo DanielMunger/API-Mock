10f06a1b26fc5978c4222ca9de3feb605320d5cd
Daniel Munger <mungerda@gmail.com>
2017-08-23 20:04:00 -0700

    comments out controller code. for later use.

--- src/API-4TELL/Controllers/CategoriesController.cs
+++ src/API-4TELL/Controllers/CategoriesController.cs
@@ -1,62 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using API_4TELL.Models;
-using System.Diagnostics;
-using API_4TELL.Models.Repositories;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using API_4TELL.Models;
+//using System.Diagnostics;
+//using API_4TELL.Models.Repositories;
 
-namespace API_4TELL.Controllers
-{
-    [Route("api/[controller]")]
-    public class CategoriesController : Controller
-    {        
+//namespace API_4TELL.Controllers
+//{
+//    [Route("api/[controller]")]
+//    public class CategoriesController : Controller
+//    {        
 
-        private ICategoryRepository categoryRepo;
+//        private ICategoryRepository categoryRepo;
 
-        public CategoriesController(ICategoryRepository categoryRepo = null)
-        {
-            if (categoryRepo == null)
-            {
-                this.categoryRepo = new EFCategoryRepository();
-            }
-            else
-            {
-                this.categoryRepo = categoryRepo;
-            }
-        }
+//        public CategoriesController(ICategoryRepository categoryRepo = null)
+//        {
+//            if (categoryRepo == null)
+//            {
+//                this.categoryRepo = new EFCategoryRepository();
+//            }
+//            else
+//            {
+//                this.categoryRepo = categoryRepo;
+//            }
+//        }
 
 
-        [HttpGet]
-        public IActionResult GetAll(string categoryName)
-        {
-            if (categoryName != null)
-            {
-                var category = categoryRepo.Categories.FirstOrDefault(t => t.CategoryName == categoryName);
-                if (category == null)
-                {
+//        [HttpGet]
+//        public IActionResult GetAll(string categoryName)
+//        {
+//            if (categoryName != null)
+//            {
+//                var category = categoryRepo.Categories.FirstOrDefault(t => t.CategoryName == categoryName);
+//                if (category == null)
+//                {
 
-                    return NotFound();
-                }
-                return new ObjectResult(category);
-            }
-            else
-            {
-                return new ObjectResult(categoryRepo.Categories.ToList());
-            }
-        }
+//                    return NotFound();
+//                }
+//                return new ObjectResult(category);
+//            }
+//            else
+//            {
+//                return new ObjectResult(categoryRepo.Categories.ToList());
+//            }
+//        }
 
-        [HttpGet("{name}", Name = "GetProducts")]
-        public IActionResult GetProducts(string name)
-        {
-            var category = categoryRepo.Categories.FirstOrDefault(t => t.CategoryName == name);
-            if (category == null)
-            {
+//        [HttpGet("{name}", Name = "GetProducts")]
+//        public IActionResult GetProducts(string name)
+//        {
+//            var category = categoryRepo.Categories.FirstOrDefault(t => t.CategoryName == name);
+//            if (category == null)
+//            {
 
-                return NotFound();
-            }
-            return new ObjectResult(category);
-        }
-    }
-}
+//                return NotFound();
+//            }
+//            return new ObjectResult(category);
+//        }
+//    }
+//}
